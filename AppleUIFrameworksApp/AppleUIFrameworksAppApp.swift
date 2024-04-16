1c4e9249fc4f9a1d414f8728591dc647d7ba7c1a
Chi-naza <ugwuokechinazajames@gmail.com>
2024-04-16 21:03:27 +0100

    Initial Commit

---
+++ AppleUIFrameworksApp/AppleUIFrameworksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleUIFrameworksAppApp.swift
+//  AppleUIFrameworksApp
+//
+//  Created by Chinaza Ugwuoke on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleUIFrameworksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
